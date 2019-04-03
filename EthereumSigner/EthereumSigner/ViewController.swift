2a66088abcfeb9194b952484f9d022a196d62a3b
OMER YASIN KOLKANAT <omer.kolkanat@turkcell.com.tr>
2019-04-03 19:02:44 +0300

    project created

---
+++ EthereumSigner/EthereumSigner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EthereumSigner
+//
+//  Created by OMER YASIN KOLKANAT on 3.04.2019.
+//  Copyright © 2019 Omer Kolkanat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
